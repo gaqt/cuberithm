c4d5fea125dcc1069fde7d912bf87a9d4f6ba447
logemii <emilybertollo@outlook.com>
2024-01-02 06:16:01 -0300

    first test worked

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use crate::{
+    cube::CubeState,
+    side::{Color, Side},
+    solver::Solver,
+};
 use std::env;
 
 pub mod cube;
+pub mod rotation;
 pub mod side;
+pub mod solver;
 
 /*
  * args format:
@@ -15,13 +22,101 @@ pub mod side;
  * next 9 chars: colors in blue face
  * next 9 chars: colors in yellow face
  *
+ * note: solved cube: WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBYYYYYYYYY
+ * 1 move from being solved: WWWWWWWWWOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBOOOYYYYYYYYY
+ *
  * then 'desired-state' and repeat for desired state
  *
- * then 'include-m' to include middle moves
- * then 'include-w' to include wide moves
- * then 'fingertricks' to exclude bad fingertrick algorithms
- * then 'max-mem' followed by the maximum amount of memory used by in megabytes
+ * then 'min-moves' followed by the minimum # of moves
+ * then 'max-moves' followed by the maximum # of moves
+ *
+ * TODO: then 'include-2' to include double moves
+ * TODO: then 'include-m' to include middle moves
+ * TODO: then 'include-w' to include wide moves
+ * TODO: then 'fingertricks' to exclude bad fingertrick algorithms
+ * TODO: then 'max-mem' followed by the maximum amount of memory used by in megabytes
  */
+
+fn get_side(side_str: &str) -> Side {
+    assert_eq!(side_str.len(), 9);
+    let mut colors: [[Color; 3]; 3] = Default::default();
+    let mut chars = side_str.chars();
+    for y in 0..3 {
+        for x in 0..3 {
+            colors[x][y] = Color::from_char(chars.next().unwrap());
+        }
+    }
+    Side { colors }
+}
+
+fn get_cube_state(cube_str: &String) -> CubeState {
+    assert_eq!(cube_str.len(), 54);
+    CubeState {
+        top: get_side(&cube_str[0..9]),
+        left: get_side(&cube_str[9..18]),
+        front: get_side(&cube_str[18..27]),
+        right: get_side(&cube_str[27..36]),
+        back: get_side(&cube_str[36..45]),
+        bottom: get_side(&cube_str[45..54]),
+    }
+}
+
+fn get_initial_state(args: &Vec<String>) -> CubeState {
+    for idx in 0..args.len() {
+        if args[idx] == "initial-state" {
+            return get_cube_state(&args[idx + 1]);
+        }
+    }
+    panic!("Initial state unspecified");
+}
+
+fn get_desired_state(args: &Vec<String>) -> CubeState {
+    for idx in 0..args.len() {
+        if args[idx] == "desired-state" {
+            return get_cube_state(&args[idx + 1]);
+        }
+    }
+    panic!("Desired state unspecified");
+}
+
+fn get_min_moves(args: &Vec<String>) -> u8 {
+    for idx in 0..args.len() {
+        if args[idx] == "min-moves" {
+            return args[idx + 1].parse::<u8>().unwrap();
+        }
+    }
+    panic!("Minimum moves unspecified");
+}
+
+fn get_max_moves(args: &Vec<String>) -> u8 {
+    for idx in 0..args.len() {
+        if args[idx] == "max-moves" {
+            return args[idx + 1].parse::<u8>().unwrap();
+        }
+    }
+    panic!("Maximum moves unspecified");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let initial_state = get_initial_state(&args);
+    let desired_state = get_desired_state(&args);
+    let min_moves = get_min_moves(&args);
+    let max_moves = get_max_moves(&args);
+
+    let mut solver = Solver::new(&initial_state, &desired_state, min_moves, max_moves);
+
+    solver.solve();
+
+    let solutions = solver.collect_solutions();
+
+    for idx in 0..solutions.len() {
+        print!("Solution {}: ", idx);
+        for rot in 0..solutions[idx].len() {
+            print!("{} ", solutions[idx][rot]);
+        }
+        println!();
+    }
+    println!("Done.");
 }
---
+++ src/solver.rs
@@ -0,0 +1,156 @@
+use std::cmp;
+use std::collections::{HashMap, HashSet};
+use strum::IntoEnumIterator;
+
+use crate::{cube::CubeState, rotation::Rotation};
+
+pub struct Solver {
+    pub middle_states: HashMap<CubeState, Vec<Rotation>>,
+    pub found_solutions: HashSet<Vec<Rotation>>,
+    pub initial_state: CubeState,
+    pub desired_state: CubeState,
+    pub min_moves: u8,
+    pub max_moves: u8,
+}
+
+impl Solver {
+    pub fn new(
+        initial_state: &CubeState,
+        desired_state: &CubeState,
+        min_moves: u8,
+        max_moves: u8,
+    ) -> Solver {
+        Solver {
+            middle_states: HashMap::new(),
+            found_solutions: HashSet::new(),
+            initial_state: *initial_state,
+            desired_state: *desired_state,
+            min_moves,
+            max_moves,
+        }
+    }
+
+    /*
+     * Goes through all possible "rotation paths" in a DFS manner,
+     * stops when reaching a solution or max(1, floor(max_moves/2)) (meet in the middle)
+     */
+    fn first_pass_(
+        &mut self,
+        state: CubeState,
+        prev_states: &mut Vec<CubeState>,
+        path: &mut Vec<Rotation>,
+    ) {
+        //dbg!(state);
+        if state == self.desired_state {
+            self.found_solutions.insert(path.clone());
+            return;
+        }
+
+        if path.len() as u8 >= cmp::max(1, self.max_moves / 2) {
+            self.middle_states.insert(state, path.clone());
+            return;
+        }
+
+        prev_states.push(state);
+
+        for rotation in Rotation::iter() {
+            match path.last() {
+                Some(prev_rot) => {
+                    if *prev_rot == rotation {
+                        continue;
+                    }
+                }
+                None => {}
+            }
+            let new_state = state.rotate(rotation);
+            if prev_states.contains(&new_state) {
+                continue;
+            }
+            path.push(rotation);
+            self.first_pass_(new_state, prev_states, path);
+            path.pop();
+        }
+
+        prev_states.pop();
+    }
+
+    pub fn first_pass(&mut self) {
+        self.first_pass_(self.initial_state, &mut Vec::new(), &mut Vec::new());
+    }
+
+    /*
+     * Goes through all possible "rotation paths" in a DFS manner
+     * stops when reaching a solution (doesnt save this time) or
+     * when reaching a previously reached state or ceil(max_moves/2) (meet in the middle)
+     */
+    fn second_pass_(
+        &mut self,
+        state: CubeState,
+        prev_states: &mut Vec<CubeState>,
+        path: &mut Vec<Rotation>,
+    ) {
+        //dbg!(state);
+        if state == self.initial_state {
+            return;
+        }
+
+        if path.len() as u8 >= (self.max_moves + 1) / 2 {
+            return;
+        }
+
+        let optional_path = self.middle_states.get(&state);
+
+        match optional_path {
+            Some(found_path) => {
+                let mut complete_path = found_path.clone();
+                for idx in (0..path.len()).rev() {
+                    complete_path.push(path[idx]);
+                }
+                self.found_solutions.insert(complete_path);
+                return;
+            }
+            None => {}
+        }
+
+        prev_states.push(state);
+
+        for rotation in Rotation::iter() {
+            match path.last() {
+                Some(prev_rot) => {
+                    if *prev_rot == rotation {
+                        continue;
+                    }
+                }
+                None => {}
+            }
+            let new_state = state.rotate(rotation);
+            if prev_states.contains(&new_state) {
+                continue;
+            };
+            path.push(rotation.reverse());
+            self.second_pass_(new_state, prev_states, path);
+            path.pop();
+        }
+
+        prev_states.pop();
+    }
+
+    pub fn second_pass(&mut self) {
+        self.second_pass_(self.desired_state, &mut Vec::new(), &mut Vec::new());
+    }
+
+    pub fn solve(&mut self) {
+        self.first_pass();
+        self.second_pass();
+    }
+
+    pub fn collect_solutions(&self) -> Vec<Vec<Rotation>> {
+        let mut solutions: Vec<Vec<Rotation>> = Vec::new();
+
+        for solution in self.found_solutions.iter() {
+            solutions.push(solution.clone());
+        }
+
+        solutions
+    }
+}
