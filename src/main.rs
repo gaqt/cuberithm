c6b8971e9d5c3ad105df512d9800250fdcb65e9c
emily <snri.atomoxetine@proton.me>
2024-02-05 06:13:46 -0300

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use clap::Parser;
+
 use crate::{cube::CubeState, solution::Solution, solver::Solver};
-use std::{collections::BTreeSet, env, time::Instant};
+use std::{collections::BTreeSet, time::Instant};
 
 pub mod cube;
 pub mod face;
@@ -7,76 +9,39 @@ pub mod rotation;
 pub mod solution;
 pub mod solver;
 
-/*
- * args format:
- * colors for the initial state (54 total) (left->right, bottom->top, white on top, green on front)
- * colors: Y (yellow), B (blue), G (green), R (red), W (white), O (orange) or N (none/unspecified)
- * 'initial-state' then
- * first 9 chars: colors in white face
- * next 9 chars: colors in orange face
- * next 9 chars: colors in green face
- * next 9 chars: colors in red face
- * next 9 chars: colors in blue face
- * next 9 chars: colors in yellow face
- *
- * note: solved cube: WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBYYYYYYYYY
- * 1 move from being solved: WWWWWWWWWOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBOOOYYYYYYYYY
- *
- * then 'desired-state' and repeat for desired state
- *
- * then 'min-moves' followed by the minimum # of moves
- * then 'max-moves' followed by the maximum # of moves
- *
- * TODO: then 'include-2' to include double moves
- * TODO: then 'include-m' to include middle moves
- * TODO: then 'include-w' to include wide moves
- * TODO: then 'fingertricks' to exclude bad fingertrick algorithms
- * TODO: then 'max-mem' followed by the maximum amount of memory used by in megabytes
- */
 
-fn get_initial_state(args: &Vec<String>) -> CubeState {
-    for idx in 0..args.len() {
-        if args[idx] == "initial-state" {
-            return CubeState::from_str(&args[idx + 1]);
-        }
-    }
-    panic!("Initial state unspecified");
-}
-
-fn get_desired_state(args: &Vec<String>) -> CubeState {
-    for idx in 0..args.len() {
-        if args[idx] == "desired-state" {
-            return CubeState::from_str(&args[idx + 1]);
-        }
-    }
-    panic!("Desired state unspecified");
-}
-
-fn get_min_moves(args: &Vec<String>) -> u8 {
-    for idx in 0..args.len() {
-        if args[idx] == "min-moves" {
-            return args[idx + 1].parse::<u8>().unwrap();
-        }
-    }
-    panic!("Minimum moves unspecified");
-}
-
-fn get_max_moves(args: &Vec<String>) -> u8 {
-    for idx in 0..args.len() {
-        if args[idx] == "max-moves" {
-            return args[idx + 1].parse::<u8>().unwrap();
-        }
-    }
-    panic!("Maximum moves unspecified");
+/// Simple algorithm generator for a 3x3x3 Rubik's Cube
+/// 
+/// Format of states passed in arguments is a 54 character long string composed of:
+/// characters: Y (yellow), B (blue), G (green), R (red), W (white), O (orange)
+/// arranged from left to right, bottom to top, in the order of faces:
+/// white -> orange -> green -> red -> blue -> yellow
+/// example: WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBYYYYYYYYY (solved cube)
+///          WWWWWWWWWOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBOOOYYYYYYYYY (after U move)
+#[derive(Parser)]
+#[command(version, about, verbatim_doc_comment)]
+struct Args {
+    /// Initial Cube state, right->left bottom->top green on front white on top
+    #[arg(short, long)]
+    initial_state: String,
+    /// Desired Cube state
+    #[arg(short, long)]
+    desired_state: String,
+    /// Min moves for algorithms to be generated
+    #[arg(long)]
+    min_moves: u8,
+    /// Max moves for algorithsm to be generated
+    #[arg(long)]
+    max_moves: u8,
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    let initial_state = get_initial_state(&args);
-    let desired_state = get_desired_state(&args);
-    let min_moves = get_min_moves(&args);
-    let max_moves = get_max_moves(&args);
+    let initial_state = CubeState::from_str(&args.initial_state);
+    let desired_state = CubeState::from_str(&args.desired_state);
+    let min_moves = args.min_moves;
+    let max_moves = args.max_moves;
 
     let mut first_pass_states: u64 = 0;
     let mut second_pass_states: u64 = 0;
